374594f79fb9ea41ecf6f074acfa94c0a80c93de
PERFECTLIN <601650418@qq.com>
2016-07-11 00:53:33 +0800

    完成了NavigationView toolbar部分

--- app/src/main/java/com/example/perfectlin/starappstore/Activity/MainActivity.java
+++ app/src/main/java/com/example/perfectlin/starappstore/Activity/MainActivity.java
@@ -1,15 +1,74 @@
 package com.example.perfectlin.starappstore.Activity;
 
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.perfectlin.starappstore.R;
 
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerlayout;
+    private NavigationView navigationview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar(); //初始化toolbar
+        initNavigation(); //初始化Navigation
+
+    }
+
+
+    private void initNavigation() {
+        drawerlayout = (DrawerLayout) findViewById(R.id.activity_main_drawerlayout);
+        navigationview = (NavigationView) findViewById(R.id.activity_main_navigationview);
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        //设置navi点击回调
+        if (navigationview != null) {
+            navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    return false;
+                }
+            });
+        }
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerlayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
